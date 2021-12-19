de75e0d08e00ee4e3e0cae29577cbe33e19fffbf
Aliaa Fathy <94761307+AliaaFathy@users.noreply.github.com>
2021-12-20 00:56:11 +0200

    Update app.component.ts

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild ,Input} from '@angular/core';import { isNull } from '@angular/compiler/src/output/output_ast';
+import { CONTEXT_NAME } from '@angular/compiler/src/render3/view/util';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Frontend';
+  title = 'server';
+   ngOnInit():void {
+  
+  }
+  text:string[]=[];
+  tex=''
+  openNav() {
+ 
+    document.getElementById("mySidenav")!.style.width="250px";
+  }
+  
+   closeNav() {
+    document.getElementById("mySidenav")!.style.width="0";
+  }
+  allmails(){
+  
+  }
+  
+  send(){
+  document.getElementById("s1")!.className="hello"
+  document.getElementById("s2")!.style.display="block";
+  
+  document.getElementById("s3")!.style.display="inline";
+  }
+  submit(){
+   
+    this.tex =(<HTMLInputElement>document.getElementById("s2")).value ;
+    document.getElementById("s2")!.style.display="none";
+    document.getElementById("s3")!.style.display="none";
+    (<HTMLInputElement>document.getElementById("s2")).value=""
+    this.text.push(this.tex)
+    console.log(this.text)
+    
+  }
+  
+  
+
+
 }
