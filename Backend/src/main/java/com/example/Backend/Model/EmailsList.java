004c1d8c8db9b3a761186c593f70f6b6fbc5744d
Engy Ibrahim <engyyyibrahimmm@gmail.com>
2021-12-20 20:42:12 +0200

    apply Singleton Design Pattern

--- Backend/src/main/java/com/example/Backend/Model/EmailsList.java
+++ Backend/src/main/java/com/example/Backend/Model/EmailsList.java
@@ -3,5 +3,13 @@ package com.example.Backend.Model;
 import java.util.HashMap;
 
 public class EmailsList {
+    private static EmailsList emailsList;
     HashMap<Integer, Email> listOfEmails;
+
+    private EmailsList() {
+
+    }
+    public EmailsList getInstance() {
+        return emailsList;
+    }
 }
--- Backend/src/main/java/com/example/Backend/Model/UsersList.java
+++ Backend/src/main/java/com/example/Backend/Model/UsersList.java
@@ -3,5 +3,13 @@ package com.example.Backend.Model;
 import java.util.HashMap;
 
 public class UsersList {
+    private static UsersList usersList;
     HashMap<Integer, User> listOfUsers;
+
+    private UsersList() {
+
+    }
+    public UsersList getInstance() {
+        return usersList;
+    }
 }
